e8abe705e81e1bf2297ced9b94b3c178cd27b463
Nikelson Exantus <nikelsone1981@hotmail.com>
2019-11-09 02:15:06 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,6 +1,7 @@
 require 'pry'
 # Write a speak_to_grandma method.
 
+
 # Whatever you say to grandma, she should respond with
 # HUH?! SPEAK UP, SONNY!
 # unless you shout it (type in all capitals).
