cf5f4a9e2bbc87ab08dddfb2a1eba34fb9cbb297
Nikelson Exantus <nikelsone1981@hotmail.com>
2019-11-09 02:20:03 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -14,7 +14,6 @@ require 'pry'
 # However if you say 'I LOVE YOU GRANDMA!', she should respond with
 # 'I LOVE YOU TOO PUMPKIN!'
 def speak_to_grandma(phrase = "Hi Grandma!")
-  binding.pry
   grandma = "HUH?! SPEAK UP, SONNY"
   if phrase == "I LOVE YOU GRANDMA!"
     puts "I LOVE YOU TOO PUMPKIN!"
