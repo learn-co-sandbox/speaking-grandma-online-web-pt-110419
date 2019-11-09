bf5ab3ec808b1fac778e16172d1715a547081184
Nikelson Exantus <nikelsone1981@hotmail.com>
2019-11-09 02:01:38 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -12,7 +12,6 @@
 # However if you say 'I LOVE YOU GRANDMA!', she should respond with
 # 'I LOVE YOU TOO PUMPKIN!'
 def speak_to_grandma(phrase = "Hi Grandma!")
-  son = "Whatever you say to grandma, she should respond with"
   grandma = "HUH?! SPEAK UP, SONNY"
   if phrase == "I LOVE YOU GRANDMA"
     puts "I LOVE YOU TOO PUMPKIN!"
